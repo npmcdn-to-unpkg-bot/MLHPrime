c643847e658ccad882ccdeb79dc310bfa8a89a5a
utk9 <utkarshlamba.2@gmail.com>
2016-08-07 00:40:23 -0400

    Fix bug that returned 404 for all routes

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const AccessToken = require('twilio-temp').AccessToken;
+const SyncGrant = AccessToken.SyncGrant;
+
+var config = require('./config');
+const randomUsername = require('./randos');
 
 var app = express();
 var server = require('http').Server(app);
@@ -19,7 +22,9 @@ var udpPort = new osc.UDPPort({
     localPort: 5002,
 });
 
+
 udpPort.open();
+
 
 
 // uncomment after placing your favicon in /public
@@ -30,40 +35,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-app.get('test', function(req, res) {
+app.get('/test', function(req, res) {
   res.send("test");
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json(err);
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json(err);
-});
-
-/*
-Generate an Access Token for a sync application user - it generates a random
-username for the client requesting a token, and takes a device ID as a query
-parameter.
-*/
 app.get('/token', (request, response) => {
   var appName = 'TwilioSyncDemo';
   var identity = randomUsername();
@@ -96,9 +72,37 @@ app.get('/token', (request, response) => {
   });
 });
 
-io.on('connection', function (socket) {
-  console.log("new connection");
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err);
+});
+
+/*
+Generate an Access Token for a sync application user - it generates a random
+username for the client requesting a token, and takes a device ID as a query
+parameter.
+*/
 
 udpPort.on("message", function (oscData) {
   if (oscData.address == "/muse/eeg") {
