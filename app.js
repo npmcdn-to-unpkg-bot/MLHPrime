ce0a4aa1c9de59440966eee2ed6d2acb80f4c362
utk9 <utkarshlamba.2@gmail.com>
2016-08-06 23:01:13 -0400

    Get muse eeg data using udp server in node

--- app.js
+++ app.js
@@ -12,9 +12,15 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var osc = require('osc');
+
+var udpPort = new osc.UDPPort({
+    localAddress: "127.0.0.1",
+    localPort: 5002,
+});
+
+udpPort.open();
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,9 +29,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,10 +48,7 @@ app.get('test', function(req, res) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.json(err);
   });
 }
 
@@ -56,13 +56,55 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  res.json(err);
+});
+
+/*
+Generate an Access Token for a sync application user - it generates a random
+username for the client requesting a token, and takes a device ID as a query
+parameter.
+*/
+app.get('/token', (request, response) => {
+  var appName = 'TwilioSyncDemo';
+  var identity = randomUsername();
+  var deviceId = request.query.device;
+
+  // Create a unique ID for the client on their current device
+  var endpointId = `${appName}:${identity}:${deviceId}`;
+
+  // Create a "grant" which enables a client to use Sync as a given user,
+  // on a given device
+  var syncGrant = new SyncGrant({
+    serviceSid: config.serviceSid,
+    endpointId: endpointId
+  });
+
+  // Create an access token which we will sign and return to the client,
+  // containing the grant we just created
+  var token = new AccessToken(
+    config.accountSid, 
+    config.apiKey, 
+    config.apiSecret
+  );
+  token.addGrant(syncGrant);
+  token.identity = identity;
+
+  // Serialize the token to a JWT string and include it in a JSON response
+  response.send({
+    identity: identity,
+    token: token.toJwt()
   });
 });
 
-//io.on('new ')
+io.on('connection', function (socket) {
+  console.log("new connection");
+});
+
+udpPort.on("message", function (oscData) {
+  if (oscData.address == "/muse/eeg") {
+    console.log(oscData.args);
+  }
+});
 
 
 module.exports = {
